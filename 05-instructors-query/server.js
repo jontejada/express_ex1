e7fba15165c7778c01411e753e5c2849a6ef8328
Jon Tejada <tejjon@gmail.com>
2015-12-08 15:50:19 -0800

    finished exercise 05

--- 05-instructors-query/server.js
+++ 05-instructors-query/server.js
@@ -3,18 +3,70 @@ var express = require('express');
 var data = require('./data.js');
 var app  = express();
 
+
+// //from https://github.com/expressjs/body-parser
+// var bodyParser = require('body-parser');
+// // create application/json parser
+// var jsonParser = bodyParser.json();
+// // create application/x-www-form-urlencoded parser
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 var instructors = [
-  {given-name: "Cho", surname: "Kim", sex: male},
-  {given-name: "Liz", surname: "Howard", sex: female},
-  {given-name: "Rosa", surname: "Choi", sex: female},
-  {given-name: "Zubair", surname: "Desai", sex: male},
-  {given-name: "Tyler", surname: "Bettilyon", sex: male}
+  {givenname: "Cho", surname: "Kim", sex: "male"},
+  {givenname: "Liz", surname: "Howard", sex: "female"},
+  {givenname: "Rosa", surname: "Choi", sex: "female"},
+  {givenname: "Zubair", surname: "Desai", sex: "male"},
+  {givenname: "Tyler", surname: "Bettilyon", sex: "male"}
 ];
 
-express.get('/', function(req, res) {
-  // write code.
+app.get('/', function(req, res) {
+	res.send('Hello, Query!');
 });
 
-express.listen(8000, function() {
+app.get('/instructors', function(req, res) {
+	var key = Object.keys(req.query)[0];
+
+	if(req.query[key]) {
+		var input = req.query[key];
+		var output = '';
+		for (var i=0; i<instructors.length; i++) {
+			if (instructors[i][key].toLowerCase() === input) {
+				output += JSON.stringify(instructors[i]);
+			}
+		}
+		res.send(output);
+	} else {
+		res.send(instructors);
+	}
+
+	// if (req.query.givenname) {
+	// 	var gn_input = req.query.givenname;
+	// 	for (var i=0; i<instructors.length; i++) {
+	// 		if (instructors[i].givenname.toLowerCase() === gn_input) {
+	// 			res.send(instructors[i]);
+	// 		}
+	// 	}
+	// } else if (req.query.surname) {
+	// 	var sn_input = req.query.surname;
+	// 	for (var i=0; i<instructors.length; i++) {
+	// 		if (instructors[i].surname.toLowerCase() === sn_input) {
+	// 			res.send(instructors[i]);
+	// 		}
+	// 	}
+	// } else if (req.query.sex) {
+	// 	var se_input = req.query.sex;
+	// 	var output = '';
+	// 	for (var i=0; i<instructors.length; i++) {
+	// 		if (instructors[i].sex.toLowerCase() === se_input) {
+	// 			output += JSON.stringify(instructors[i]);
+	// 		}
+	// 	}
+	// 	res.send(output);
+	// } else {
+	// 	res.send(instructors);
+	// }
+});
+
+app.listen(8000, function() {
 	console.log("I'm listening on port 8000...");
 });
