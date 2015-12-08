f84892f5cdb45a35ef72edf7ed830066ff6b30cf
Jon Tejada <tejjon@gmail.com>
2015-12-08 11:54:01 -0800

    1st ex work

--- 01-hello-param/server.js
+++ 01-hello-param/server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function(req,res) {
-	//
+	res.send('Hello, Express!');
+});
+
+app.get('/jon', function(req,res) {
+	res.send('Hello, Jon!');
 });
 
 app.listen(8000, function() {
